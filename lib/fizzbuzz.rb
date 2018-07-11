6c90a6af73f88f5a259aee5772e661667efa2a5f
NataliaColome <nmcolome@gmail.com>
2018-07-11 00:45:53 -0600

    test and functionality to print numbers from 1 to 100

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,16 @@
+=begin
+Write a program that prints the numbers from 1 to 100 with the following rules:
+For any number that is a multiple of 3, print 'Fizz'
+For any number that is a multiple of 5, print 'Buzz'
+For any number that is a multiple of both 3 and 5, print 'FizzBuzz'
+For all other numbers, print the number.
+The output of you program will look something like this:
+=end
+
 class FizzBuzz
-  
+  attr_accessor :range
+
+  def initialize
+    @range = (1..100).to_a
+  end
 end
--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -11,4 +11,11 @@ class FizzBuzzTest < Minitest::Test
   def test_it_exists
     assert_instance_of FizzBuzz, @fizzbuzz
   end
+
+  def test_it_generates_to_100
+    assert_equal 100, @fizzbuzz.range.length
+    assert_equal 15, @fizzbuzz.range[14]
+    assert_equal 43, @fizzbuzz.range[42]
+    assert_equal 95, @fizzbuzz.range[94]
+  end
 end
