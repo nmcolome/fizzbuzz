a4c45b5d752d36aba7b763ff0bc3a97b0bc59ea2
NataliaColome <nmcolome@gmail.com>
2018-07-11 10:57:39 -0600

    Refactor test: add fizz.transform as setup

--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -6,6 +6,7 @@ require '../lib/fizzbuzz.rb'
 class FizzBuzzTest < Minitest::Test
   def setup
     @fizzbuzz = FizzBuzz.new
+    @fizz = @fizzbuzz.transform
   end
 
   def test_it_exists
@@ -14,41 +15,37 @@ class FizzBuzzTest < Minitest::Test
 
   def test_it_prints_1_to_100
     assert_equal 100, @fizzbuzz.range.length
-    # assert_equal 15, @fizzbuzz.range[14]
+    assert_equal 15, @fizzbuzz.range[14]
     assert_equal 43, @fizzbuzz.range[42]
     assert_equal 95, @fizzbuzz.range[94]
   end
 
   def test_prints_fizz_for_multiples_of_3
-    fizz = @fizzbuzz.transform
-    assert_equal "Fizz", fizz[2]
-    assert_equal "Fizz", fizz[50]
-    assert_equal "Fizz", fizz[98]
+    assert_equal "Fizz", @fizz[2]
+    assert_equal "Fizz", @fizz[50]
+    assert_equal "Fizz", @fizz[98]
   end
 
   def test_prints_buzz_for_multiples_of_5
-    fizz = @fizzbuzz.transform
-    assert_equal "Buzz", fizz[4]
-    assert_equal "Buzz", fizz[49]
-    assert_equal "Buzz", fizz[99]
+    assert_equal "Buzz", @fizz[4]
+    assert_equal "Buzz", @fizz[49]
+    assert_equal "Buzz", @fizz[99]
   end
 
   def test_prints_fizzbuzz_for_multiples_of_3_and_5
-    fizz = @fizzbuzz.transform
-    assert_equal "FizzBuzz", fizz[14]
-    assert_equal "FizzBuzz", fizz[29]
-    assert_equal "FizzBuzz", fizz[89]
+    assert_equal "FizzBuzz", @fizz[14]
+    assert_equal "FizzBuzz", @fizz[29]
+    assert_equal "FizzBuzz", @fizz[89]
   end
 
   def test_it_prints_numbers_for_any_other_case
-    fizz = @fizzbuzz.transform
     first_15 = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
     last_three = [98, "Fizz", "Buzz"]
  
-    assert_equal 19, fizz[18]
-    assert_equal 52, fizz[51]
-    assert_equal 97, fizz[96]
-    assert_equal first_15, fizz[0..14]
-    assert_equal last_three, fizz[-3..-1]
+    assert_equal 19, @fizz[18]
+    assert_equal 52, @fizz[51]
+    assert_equal 97, @fizz[96]
+    assert_equal first_15, @fizz[0..14]
+    assert_equal last_three, @fizz[-3..-1]
   end
 end
