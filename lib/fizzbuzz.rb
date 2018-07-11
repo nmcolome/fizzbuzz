12bc178a5fa60d070ea68c233b1d356fd0239440
NataliaColome <nmcolome@gmail.com>
2018-07-11 10:16:55 -0600

    test and funcionality to print 'Fizz' if is multiple of 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,9 @@
 =begin
-Write a program that prints the numbers from 1 to 100 with the following rules:
-For any number that is a multiple of 3, print 'Fizz'
-For any number that is a multiple of 5, print 'Buzz'
-For any number that is a multiple of both 3 and 5, print 'FizzBuzz'
-For all other numbers, print the number.
-The output of you program will look something like this:
+[x] Write a program that prints the numbers from 1 to 100 with the following rules:
+[x] For any number that is a multiple of 3, print 'Fizz'
+[] For any number that is a multiple of 5, print 'Buzz'
+[] For any number that is a multiple of both 3 and 5, print 'FizzBuzz'
+[] For all other numbers, print the number.
 =end
 
 class FizzBuzz
@@ -12,5 +11,13 @@ class FizzBuzz
 
   def initialize
     @range = (1..100).to_a
+  end
+
+  def transform
+    @range.map do |element|
+      if element % 3 == 0
+        element = "Fizz"
+      end
+    end
   end
 end
--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -12,10 +12,17 @@ class FizzBuzzTest < Minitest::Test
     assert_instance_of FizzBuzz, @fizzbuzz
   end
 
-  def test_it_generates_to_100
+  def test_it_prints_1_to_100
     assert_equal 100, @fizzbuzz.range.length
-    assert_equal 15, @fizzbuzz.range[14]
+    # assert_equal 15, @fizzbuzz.range[14]
     assert_equal 43, @fizzbuzz.range[42]
     assert_equal 95, @fizzbuzz.range[94]
+  end
+
+  def test_prints_fizz_for_multiples_of_3
+    fizz = @fizzbuzz.transform
+    assert_equal "Fizz", fizz[2]
+    assert_equal "Fizz", fizz[50]
+    assert_equal "Fizz", fizz[98]
   end
 end
